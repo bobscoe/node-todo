455d54238bf4767fdf76cc8e455d545f299df2bc
bobscoe <mjg5kc@mail.missouri.edu>
2016-12-06 02:40:00 +0000

    update database config to point to mongoDB

--- config/database.js
+++ config/database.js
@@ -1,4 +1,7 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    localUrl : 'mongodb://dumAdmin:dumAss@ec2-35-165-79-161.us-west-2.compute.amazonaws.com:27017/dummyDB',
+    blankUrl : null
+    
+    //localUrl: 'mongodb://localhost/meanstacktutorials'
 };
